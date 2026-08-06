472ad4c73c3369f624c714ff4ba19a2c140923da
Navi2004725 <naveesh.a@artecx-solutions.com>
2026-08-06 16:38:58 +0530

    added cloudinary service✏️

--- src/main/java/com/example/touristWebsite/service/impl/impl/HotelImageServiceImpl.java
+++ src/main/java/com/example/touristWebsite/service/impl/impl/HotelImageServiceImpl.java
@@ -6,81 +6,135 @@ import com.example.touristWebsite.entity.HotelEntity;
 import com.example.touristWebsite.entity.HotelImageEntity;
 import com.example.touristWebsite.repo.HotelImageRepository;
 import com.example.touristWebsite.repo.HotelRepository;
-import com.example.touristWebsite.service.impl.FileStorageService;
+import com.example.touristWebsite.service.impl.CloudinaryService;
 import com.example.touristWebsite.service.impl.HotelImageService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class HotelImageServiceImpl implements HotelImageService {
+
+
     private final ModelMapper modelMapper;
+    private final CloudinaryService cloudinaryService;
+    private final HotelRepository hotelRepo;
+    private final HotelImageRepository imageRepo;
 
-    public HotelImageServiceImpl(ModelMapper modelMapper) {
+
+    public HotelImageServiceImpl(
+            ModelMapper modelMapper,
+            CloudinaryService cloudinaryService,
+            HotelRepository hotelRepo,
+            HotelImageRepository imageRepo
+    ) {
         this.modelMapper = modelMapper;
+        this.cloudinaryService = cloudinaryService;
+        this.hotelRepo = hotelRepo;
+        this.imageRepo = imageRepo;
     }
-    @Autowired
-    private FileStorageService fileStorageService;
 
-    @Autowired
-    private HotelRepository hotelRepo;
 
-    @Autowired
-    private HotelImageRepository imageRepo;
 
     @Override
     public String uploadImage(int hotelId, MultipartFile file) {
+
         try {
+
             HotelEntity hotel = hotelRepo.findById(hotelId)
-                    .orElseThrow(() -> new RuntimeException("Hotel not found"));
+                    .orElseThrow(() ->
+                            new RuntimeException("Hotel not found")
+                    );
 
-            // Save file in uploads folder
-            String filePath = fileStorageService.saveFile(file);
 
-            // Save image entity in DB
+            // Upload to Cloudinary
+            String imageUrl = cloudinaryService.upload(file);
+
+
+            // Save URL to database
             HotelImageEntity image = new HotelImageEntity();
-            image.setUrl(filePath);
-            image.setHotel(hotel);
-            imageRepo.save(image);
-            return filePath;
-        } catch (Exception e) {
-                throw new RuntimeException(e);
-        }
 
+            image.setUrl(imageUrl);
+            image.setHotel(hotel);
+
+            imageRepo.save(image);
+
+
+            return imageUrl;
+
+
+        } catch (Exception e) {
+
+            throw new RuntimeException(
+                    "Image upload failed",
+                    e
+            );
+        }
     }
+
+
 
     @Override
     public HotelImageResponseDTO deleteImage(int hotelId, int imageId) {
+
         HotelEntity hotel = hotelRepo.findById(hotelId)
-                .orElseThrow(() -> new RuntimeException("Hotel not found"));
+                .orElseThrow(() ->
+                        new RuntimeException("Hotel not found")
+                );
+
 
         HotelImageEntity image = imageRepo.findById(imageId)
-                .orElseThrow(() -> new RuntimeException("Image not found"));
+                .orElseThrow(() ->
+                        new RuntimeException("Image not found")
+                );
 
-        if (image.getHotel().getHotelId() != (hotel.getHotelId())) {
-            throw new RuntimeException("Image does not belong to the specified hotel");
+
+        if (image.getHotel().getHotelId() != hotel.getHotelId()) {
+            throw new RuntimeException(
+                    "Image does not belong to this hotel"
+            );
         }
+
 
         imageRepo.delete(image);
 
-        return new HotelImageResponseDTO(image.getImageId(), image.getUrl());
+
+        return new HotelImageResponseDTO(
+                image.getImageId(),
+                image.getUrl()
+        );
     }
+
+
 
     @Override
     public List<GetHotelImageDTO> getAllImages() {
-        List<HotelImageEntity> hotelImages = imageRepo.findAll();
-        List<GetHotelImageDTO> addImages = new ArrayList<>();
-        if(hotelImages != null) {
-            for (HotelImageEntity hotelImageEntity : hotelImages) {
-                GetHotelImageDTO imageDTO = modelMapper.map(hotelImageEntity, GetHotelImageDTO.class);
-                addImages.add(imageDTO);
-            }
-            return addImages;
+
+
+        List<HotelImageEntity> hotelImages =
+                imageRepo.findAll();
+
+
+        List<GetHotelImageDTO> addImages =
+                new ArrayList<>();
+
+
+        for (HotelImageEntity hotelImage : hotelImages) {
+
+            GetHotelImageDTO imageDTO =
+                    modelMapper.map(
+                            hotelImage,
+                            GetHotelImageDTO.class
+                    );
+
+            addImages.add(imageDTO);
         }
-        return null;
+
+
+        return addImages;
     }
 }
--- src/main/java/com/example/touristWebsite/service/impl/impl/RoomImageServiceImpl.java
+++ src/main/java/com/example/touristWebsite/service/impl/impl/RoomImageServiceImpl.java
@@ -1,43 +1,75 @@
 package com.example.touristWebsite.service.impl.impl;
 
-import com.example.touristWebsite.entity.HotelEntity;
 import com.example.touristWebsite.entity.RoomEntity;
 import com.example.touristWebsite.entity.RoomImageEntity;
 import com.example.touristWebsite.repo.RoomImageRepository;
 import com.example.touristWebsite.repo.RoomRepository;
-import com.example.touristWebsite.service.impl.FileStorageService;
+import com.example.touristWebsite.service.impl.CloudinaryService;
 import com.example.touristWebsite.service.impl.RoomImageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
 
 @Service
 public class RoomImageServiceImpl implements RoomImageService {
-    @Autowired
-    private RoomImageRepository roomImageRepository;
-    @Autowired
-    private RoomRepository roomRepository;
-    @Autowired
-    private FileStorageService fileStorageService;
+
+
+    private final RoomImageRepository roomImageRepository;
+    private final RoomRepository roomRepository;
+    private final CloudinaryService cloudinaryService;
+
+
+
+    public RoomImageServiceImpl(
+            RoomImageRepository roomImageRepository,
+            RoomRepository roomRepository,
+            CloudinaryService cloudinaryService
+    ) {
+        this.roomImageRepository = roomImageRepository;
+        this.roomRepository = roomRepository;
+        this.cloudinaryService = cloudinaryService;
+    }
+
+
 
     @Override
     public String uploadImage(Long id, MultipartFile file) {
-        try {
-            RoomEntity room = roomRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Room not found"));
-            String filePath = fileStorageService.saveFile(file);
 
-            RoomImageEntity roomImageEntity = new RoomImageEntity();
-            roomImageEntity.setUrl(filePath);
-            roomImageEntity.setRoom(room);
-            roomImageRepository.save(roomImageEntity);
-            return filePath;
+        try {
+
+            RoomEntity room =
+                    roomRepository.findById(id)
+                            .orElseThrow(() ->
+                                    new RuntimeException("Room not found")
+                            );
+
+
+            // Upload image to Cloudinary
+            String imageUrl =
+                    cloudinaryService.upload(file);
+
+
+
+            RoomImageEntity roomImage =
+                    new RoomImageEntity();
+
+
+            roomImage.setUrl(imageUrl);
+            roomImage.setRoom(room);
+
+
+            roomImageRepository.save(roomImage);
+
+
+            return imageUrl;
+
 
         } catch (Exception e) {
-                throw new RuntimeException(e);
-        }
 
+            throw new RuntimeException(
+                    "Image upload failed",
+                    e
+            );
+        }
     }
 }
